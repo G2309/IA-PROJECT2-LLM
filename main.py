59a5721f3fef3299ca317546e9ebd2bd6069dd72
gustavo <cru22779@uvg.edu.gt>
2024-11-17 23:00:04 -0600

    Final lab08

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from typing import Any, Dict
 from dotenv import load_dotenv
 from langchain import hub
-from langchain_experimental.agents import create_csv_agent
+from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents import create_csv_agent
+import os
 
 load_dotenv()
 
 def main():
     print("Start...")
+
+    os.makedirs("/app/output", exist_ok=True)
 
     instructions = """
     You are an agent designed to write and execute Python code to answer questions.
@@ -24,27 +29,59 @@ def main():
 
     tools = [PythonREPLTool()]
 
-    agent = create_react_agent(
+    python_agent = create_react_agent(
         prompt=prompt,
         llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
         tools=tools,    
     )
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    python_agent_executor = AgentExecutor(agent=python_agent, tools=tools, verbose=True)
 
-    csv_agent = create_csv_agent(
+    csv_agent_executor: AgentExecutor = create_csv_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4"),
         path="episode_info.csv",
         verbose=True,
         allow_dangerous_code=True,
     )
 
-    csv_agent.invoke(
-        input={
-            "input": "How many columns are there in the file? episode_info.csv?"
-        }
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str,Any]:
+        return python_agent_executor.invoke({"input":original_prompt})
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor_wrapper,
+            description="""
+                useful when you need to transform natural language to python and execute the pythno code, returning the results of the code execution 
+                DOES NOT ACCEPT CODE AS INPUT
+            """,
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent_executor.invoke,
+            description="""
+                useful when you need to answer questions over episode_info.csv file,
+                takes as an input the entire question and returns the answer running pandas calculations
+            """,
+        ),
+    ]
+    prompt=base_prompt.partial(instructions="")
+    grand_agent=create_react_agent(
+            prompt=prompt,
+            llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+            tools=tools,
+    )
+    grand_agent_executor = AgentExecutor(
+            agent=grand_agent,
+            tools=tools,
+            verbose=True
     )
 
-
+    print(
+            grand_agent_executor.invoke({
+                "input": "Read the following csv and tell me 1. which season has the most episodes, 2. which season has the fewest episodes"
+            #"input": "Generate and save in /app/output 1 qrcode that targets: 'https://www.youtube.com/watch?v=eGwkLbFtrNo'"
+                })
+    )
 
 if __name__ =="__main__":
     main()
